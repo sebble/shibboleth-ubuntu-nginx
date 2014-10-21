4fb433c15a8020316f78292149e34aade36a8601
Sebastian Mellor (resviz) <s.j.i.mellor@newcastle.ac.uk>
2014-10-21 11:24:48 +0100

    install

--- build/00_fetch.sh
+++ build/00_fetch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd tmp
+
 wget http://shibboleth.net/downloads/log4shib/latest/log4shib-1.0.8.tar.gz && tar xzf log4shib-1.0.8.tar.gz
 wget http://mirror.vorboss.net/apache//xerces/c/3/sources/xerces-c-3.1.1.tar.gz && tar xzf xerces-c-3.1.1.tar.gz
 wget http://mirror.vorboss.net/apache//xerces/c/3/sources/xerces-c-tools-3.1.1.tar.gz && tar xzf xerces-c-tools-3.1.1.tar.gz
---
+++ build/02_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install liblog4shib1 libmemcached10 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6;
