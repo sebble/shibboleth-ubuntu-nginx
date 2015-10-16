1ff1419a737d66786d8345e5234ef8a98c18c4a6
Sebastian Mellor <github@sebble.com>
2015-10-16 10:36:12 +0100

    non-interactive needs '-y'

--- build/02_install.sh
+++ build/02_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-sudo apt-get install liblog4shib1 libmemcached10 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6;
+sudo apt-get -y install liblog4shib1 libmemcached10 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6;
