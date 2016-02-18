0b58236322e27888f176c359d9eba57579cbc988
Sebastian Mellor <github@sebble.com>
2016-02-18 16:12:05 +0000

    testing debain:jessie

--- build/02_install.sh
+++ build/02_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 set -e
-sudo apt-get -y install liblog4shib1 libmemcached10 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6;
+sudo apt-get -y install liblog4shib1 libmemcached10 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6 ||
+sudo apt-get -y install liblog4shib1 libmemcached11 libsaml8 libshibsp6 libxerces-c3.1 libxmltooling6;
