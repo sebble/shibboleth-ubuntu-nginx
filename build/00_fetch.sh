6cf9c22fcee78fe3c440732d5e0701e09ecc1bf5
Sebastian Mellor <github@sebble.com>
2016-02-18 16:15:57 +0000

    update xerces part 1/2

--- build/00_fetch.sh
+++ build/00_fetch.sh
@@ -6,8 +6,8 @@ mkdir -p tmp
 cd tmp
 
 wget http://shibboleth.net/downloads/log4shib/latest/log4shib-1.0.9.tar.gz && tar xzf log4shib-1.0.9.tar.gz
-wget http://mirror.vorboss.net/apache/xerces/c/3/sources/xerces-c-3.1.2.tar.gz && tar xzf xerces-c-3.1.2.tar.gz
-wget http://mirror.vorboss.net/apache/xerces/c/3/sources/xerces-c-tools-3.1.2.tar.gz && tar xzf xerces-c-tools-3.1.2.tar.gz
+wget http://mirror.vorboss.net/apache/xerces/c/3/sources/xerces-c-3.1.3.tar.gz && tar xzf xerces-c-3.1.3.tar.gz
+wget http://mirror.vorboss.net/apache/xerces/c/3/sources/xerces-c-tools-3.1.3.tar.gz && tar xzf xerces-c-tools-3.1.3.tar.gz
 wget http://www.mirrorservice.org/sites/ftp.apache.org/santuario/c-library/xml-security-c-1.7.3.tar.gz && tar xzf xml-security-c-1.7.3.tar.gz
 wget http://shibboleth.net/downloads/c++-opensaml/latest/xmltooling-1.5.5.tar.gz && tar xzf xmltooling-1.5.5.tar.gz
 wget http://shibboleth.net/downloads/c++-opensaml/latest/opensaml-2.5.5.tar.gz && tar xzf opensaml-2.5.5.tar.gz
